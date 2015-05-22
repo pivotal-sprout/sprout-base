cfac611a3515fe52e90d73403036403b487b1c33
Hua Zhang <zhuadl@cn.ibm.com>
2015-05-21 17:20:28 -0700

    Adds bash_it plugin to setup direnv
    Also bumps cookbooks minor version to reflect new feature
    Signed-off-by: Jonathan Barnes <jbarnes@pivotal.io>

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'sprout-maintainers@pivotallabs.com'
 license          'MIT'
 description      'A handful of things every Sprout recipe needs'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 supports         'mac_os_x'
