c077e7723f50359dd8d5fcd46a120999636765ad
Aram Price <aprice@pivotal.io>
2015-10-08 12:06:07 -0700

    Bash-it recipe now prepends both /usr/local/bin and .../sbin
    * this should make `brew doctor` happy
    
    Fixes #5

--- attributes/bash_it.rb
+++ attributes/bash_it.rb
@@ -12,7 +12,7 @@ node.default['bash_it'] ={
       "sprout-base" => %w[
         bash_it/custom/disable_ctrl-s_output_control.bash
         bash_it/custom/enable_ctrl-o_history_execution.bash
-        bash_it/custom/ensure_usr_local_bin_first.bash
+        bash_it/custom/ensure_usr_local_dirs_first.bash
         bash_it/custom/add_user_initials_to_git_prompt_info.bash
       ]
   },
