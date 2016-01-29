217fdf913f8f11dcf99d03ab490b19ef62d56b55
Dan Wendorf <dwendorf@pivotal.io>
2016-01-29 12:33:42 -0800

    Bump version to 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'sprout-maintainers@googlegroups.com'
 license          'MIT'
 description      'A handful of things every Sprout recipe needs'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 supports         'mac_os_x'
 depends          'homebrew'
