a7f515dd6ec08e682c9fb0637c8a153fa275e19f
aram price <aprice@pivotal.io>
2018-04-27 12:11:53 -0400

    Bump version to 0.7.0

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-chef_version '>= 12.6.0'
-name 'sprout-base'
-maintainer 'Pivotal Labs'
-maintainer_email 'sprout-maintainers@googlegroups.com'
-issues_url 'https://github.com/pivotal-sprout/sprout-base/issues'
-source_url 'https://github.com/pivotal-sprout/sprout-base'
-license 'MIT'
-description 'A handful of things every Sprout recipe needs'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.6.0'
-supports 'mac_os_x'
-depends 'homebrew'
+chef_version      '>= 12.6.0'
+name              'sprout-base'
+maintainer        'Pivotal Labs'
+maintainer_email  'sprout-maintainers@googlegroups.com'
+issues_url        'https://github.com/pivotal-sprout/sprout-base/issues'
+source_url        'https://github.com/pivotal-sprout/sprout-base'
+license           'MIT'
+description       'A handful of things every Sprout recipe needs'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.7.0'
+supports          'mac_os_x'
+depends           'homebrew'
